d3273898d406cfb9806a452865ee0eaa8ccb8484
svintkovska <tanyasv97@gmail.com>
2022-12-17 20:05:21 +0200

    working chat

---
+++ ChatClientWPF/dto/UploadResponseDTO.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChatClientWPF.dto
+{
+    public class UploadResponseDTO
+    {
+        public string Image { get; set; }
+    }
+}
