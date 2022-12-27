7c189baf61f0c8e67f5a424e4220af912301f769
svintkovska <tanyasv97@gmail.com>
2022-12-27 13:38:41 +0200

    add repositories

---
+++ DAL/Repositories/MessageRepository.cs
@@ -0,0 +1,46 @@
+﻿using DAL.Data;
+using DAL.Data.Entities;
+using DAL.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DAL.Repositories
+{
+    public class MessageRepository : IRepository<MessageEntity>
+    {
+        private AppEFContext _context = new AppEFContext();
+
+        public void Create(MessageEntity item)
+        {
+            if (item != null)
+            {
+                _context.Messages.Add(item);
+                _context.SaveChanges();
+            }
+        }
+
+        public void Delete(int? id)
+        {
+            var tempMessage = _context.Messages.Find(id);
+            if (tempMessage != null)
+            {
+                _context.Messages.Remove(tempMessage);
+                _context.SaveChanges();
+            }
+        }
+
+        public MessageEntity Find(int? id) => _context.Messages.Find(id);
+
+        public IEnumerable<MessageEntity> GetAll() => _context.Messages;
+
+        public void Update(MessageEntity item)
+        {
+            var tempMessage = _context.Messages.Find(item.Id);
+            tempMessage.Text = item.Text;
+            tempMessage.Time = item.Time;
+            tempMessage.UserId = item.UserId;
+            _context.SaveChanges();
+        }
+    }
+}
--- DAL/Repositories/UserRepository.cs
+++ DAL/Repositories/UserRepository.cs
@@ -1,10 +1,46 @@
-﻿using System;
+﻿using DAL.Data;
+using DAL.Interfaces;
+using SomeeMSSQLConsole.Data.Entities;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace DAL.Repositories
 {
-    public class UserRepository
+    public class UserRepository : IRepository<UserEntity>
     {
+        private AppEFContext _context = new AppEFContext();
+
+        public void Create(UserEntity item)
+        {
+            if (item != null)
+            {
+                _context.Users.Add(item);
+                _context.SaveChanges();
+            }
+        }
+
+        public void Delete(int? id)
+        {
+            var tempUser = _context.Users.Find(id);
+            if (tempUser != null)
+            {
+                _context.Users.Remove(tempUser);
+                _context.SaveChanges();
+            }
+        }
+
+        public UserEntity Find(int? id) => _context.Users.Find(id);
+
+        public IEnumerable<UserEntity> GetAll() => _context.Users;
+
+        public void Update(UserEntity item)
+        {
+            var tempUser = _context.Users.Find(item.Id);
+            tempUser.Name = item.Name;
+            tempUser.Email = item.Email;
+            tempUser.Password = item.Password;
+            _context.SaveChanges();
+        }
     }
 }
